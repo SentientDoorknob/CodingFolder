9304806af98b18b0c89fe911a5225e05fa1c150f
SentientDoorknob <justanotherphoenixmaybe@gmail.com>
2023-12-02 20:38:06 +0000

    improved readablity of vingere cipher decoder

--- Cryptography/VingereCipher/vingereKeywordLength.py
+++ Cryptography/VingereCipher/vingereKeywordLength.py
@@ -3,26 +3,26 @@ sys.path.append("C:\\Users\\justa\\Documents\\Coding\\CodingFolder\\Cryptography
 
 import cryptotools
 
-ENG_IC = 0.0686
-
 def getEstimatedKeywordLength(text, maxKeyLength):
     differences = []
-    smallestDifference = 1
     index = 0
 
+    # iterate over possible keyword lengths (assumptions)
     for i in range(1,maxKeyLength+1):
-        cosets = cryptotools.splitCosets(i, text)
+        cosets = cryptotools.splitCosets(i, text) # split into relevant cosets on assumed keyword length
 
+        # take the average of the index of coinceidenceseses of each coset
         IC_sum = 0
         for coset in cosets:
             coset_IC = cryptotools.calculateIC(coset)
             IC_sum += coset_IC
-
         IC_sum /= i
-        dif = abs(ENG_IC - IC_sum)
-        if dif < smallestDifference : smallestDifference = dif; index = i
 
+        # create a list of the differences between IoC of english and that of cosets at assumed keyword length
+        # correct keyword length should have a vaule of around 0.001 / 0.002
+        # incorrect keyword length should have a value of around 0.01 / 0.02
+        dif = abs(cryptotools.ENG_IC - IC_sum)
         differences.append(dif)
 
-    return index, differences
+    return differences
         
